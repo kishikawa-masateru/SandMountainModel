4f314814a832fdbe499c1146ffb646ca110ccb49
masateru kishikawa <kishimasa0614@gmail.com>
2020-09-24 18:52:00 +0900

    モデル化に必要な最低限の処理を追加

--- src/mountain/MountainSquare.java
+++ src/mountain/MountainSquare.java
@@ -7,4 +7,22 @@ package mountain;
  */
 public class MountainSquare {
 
+	private int[][] square;		//	上から見た砂山の各地点
+
+	public MountainSquare(int x, int y) {
+		// square を初期化
+		square = new int[y][x];
+		for (int Y = 0; Y < y; Y++) {
+			for (int X = 0; X < x; X++) {
+				square[y][x] = 0;
+			}
+		}
+	}
+
+	/**
+	 * (x, y) の数値を1つ増やす
+	 */
+	public void increase(int x, int y) {
+		square[y][x]++;
+	}
 }
