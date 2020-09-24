c327a06dadf9e25124d99b8e86ff60ce9c54613b
masateru kishikawa <kishimasa0614@gmail.com>
2020-09-25 02:40:48 +0900

    .gitignoreを修正

--- src/mountain/MountainSquare.java
+++ src/mountain/MountainSquare.java
@@ -14,7 +14,7 @@ public class MountainSquare {
 		square = new int[y][x];
 		for (int Y = 0; Y < y; Y++) {
 			for (int X = 0; X < x; X++) {
-				square[y][x] = 0;
+				square[Y][X] = 0;
 			}
 		}
 	}
@@ -24,5 +24,73 @@ public class MountainSquare {
 	 */
 	public void increase(int x, int y) {
 		square[y][x]++;
+
+		if (square[y][x] == 4) {
+			// (x, y) の値が4になったら，以下の処理を実行する
+			// (x, y) の値を0にする
+			square[y][x] = 0;
+
+			// 周囲の数値を+1する
+			increaseAround(x, y);
+		}
+	}
+
+	/**
+	 * (x, y) の周囲の数値を1つ増やす
+	 * @param x
+	 * @param y
+	 */
+	private void increaseAround(int x, int y) {
+		// (x - 1, y) の数値を+1
+		if (canIncrease(x - 1, y))
+			increase(x - 1, y);
+
+		// (x, y - 1) の数値を+1
+		if (canIncrease(x, y - 1))
+			increase(x, y - 1);
+
+		// (x + 1, y) の数値を+1
+		if (canIncrease(x + 1, y))
+			increase(x + 1, y);
+
+		// (x, y + 1) の数値を+1
+		if (canIncrease(x, y + 1))
+			increase(x, y + 1);
+	}
+
+	/**
+	 * (x, y)の座標をインクリメントできるか
+	 * @param x
+	 * @param y
+	 * @return インクリメントできるとtrue<br>引数の値が，行列の範囲がだったらfalse<br>
+	 */
+	private boolean canIncrease(int x, int y) {
+		if (x < 0 || x > square[0].length || y < 0 || y > square.length)
+			return false;
+
+		return true;
+	}
+
+	/**
+	 * @return 砂山の各地点
+	 */
+	public int[][] getSquare() {
+		return square;
+	}
+
+	/**
+	 * @return 各座標の値
+	 */
+	@Override
+	public String toString() {
+		StringBuilder builder = new StringBuilder();
+		for (int y = 0; y < square.length; y++) {
+			for (int x = 0; x < square[0].length; x++) {
+				builder.append(square[y][x]).append(" , ");
+			}
+			builder.replace(builder.length() - 3, builder.length(), "\n");
+		}
+
+		return builder.toString();
 	}
 }
