0eaf48f67d79dd95fff9f9f4279e8160b59a7af2
masateru kishikawa <kishimasa0614@gmail.com>
2020-09-26 05:42:43 +0900

    add: MountainSquareクラスのテストコードを作成
    はじめに，MountainSquareクラスのcanIncreaseのテストケースを作成

---
+++ test/mountain/MountainSquareTest.java
@@ -0,0 +1,24 @@
+package mountain;
+
+import org.junit.jupiter.api.Test;
+
+/**
+ * MountainSquareのテストコード
+ * @author masateru
+ *
+ */
+class MountainSquareTest {
+
+	private final int COLUMNS = 10;		// 行数
+	private final int ROWS = 10;		// 列数
+
+	/**
+	 * アクセスできないx座標の値を指定したときのテスト<br>
+	 * y座標は正しい値
+	 *
+	 */
+	@Test
+	void tc1_canIncrease() {
+	}
+
+}
