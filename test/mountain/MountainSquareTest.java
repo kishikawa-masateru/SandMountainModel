7ecd391334d9326acc079c444d559b7f40ba77f0
masateru kishikawa <kishimasa0614@gmail.com>
2020-09-27 00:24:32 +0900

    add: canIncreaseのテストケースに対応したテストメソッドを追加

--- test/mountain/MountainSquareTest.java
+++ test/mountain/MountainSquareTest.java
@@ -27,15 +27,15 @@ class MountainSquareTest {
 
 		// 列数で例外スロー
 		assertThrows(InvalidArgumentException.class, () -> new MountainSquare(-1, 10));
-		assertThrows(InvalidArgumentException.class, () -> new MountainSquare(0, 10));
+		assertThrows(InvalidArgumentException.class, () -> new MountainSquare(0, 10));	// 列数の境界値
 
 		// 行数で例外スロー
 		assertThrows(InvalidArgumentException.class, () -> new MountainSquare(10, 0));
-		assertThrows(InvalidArgumentException.class, () -> new MountainSquare(10, -1));
+		assertThrows(InvalidArgumentException.class, () -> new MountainSquare(10, -1));	// 行数の境界値
 	}
 
 	/**
-	 * アクセスできないx座標の値を指定したときのテスト<br>
+	 * 範囲外のx座標を指定したときのテスト<br>
 	 * y座標は正しい値
 	 *
 	 * @throws SecurityException
@@ -60,4 +60,49 @@ class MountainSquareTest {
 		assertFalse((boolean) canIncrease.invoke(square, ROWS + 1, 0));	// y座標は正しい値
 	}
 
+	/**
+	 * 範囲外のy座標の値を指定したときのテスト
+	 * x座標は正しい値
+	 *
+	 * @throws NoSuchMethodException
+	 * @throws SecurityException
+	 * @throws IllegalAccessException
+	 * @throws IllegalArgumentException
+	 * @throws InvocationTargetException
+	 * @throws InvalidArgumentException
+	 */
+	@Test
+	void tc2_canIncrease()
+			throws NoSuchMethodException, SecurityException, IllegalAccessException, IllegalArgumentException,
+			InvocationTargetException, InvalidArgumentException {
+
+		MountainSquare square = new MountainSquare(COLUMNS, ROWS);
+
+		Method canIncrease = MountainSquare.class.getDeclaredMethod("canIncrease", int.class, int.class);
+		canIncrease.setAccessible(true);
+
+		assertFalse((boolean) canIncrease.invoke(square, 2, -1));		// x座標は正しい値
+		assertFalse((boolean) canIncrease.invoke(square, 8, ROWS + 1));	// x座標は正しい値
+	}
+
+	/**
+	 * 範囲内のx，y座標を指定したときのテスト
+	 */
+	@Test
+	void tc3_canIncrease()
+			throws NoSuchMethodException, SecurityException, IllegalAccessException, IllegalArgumentException,
+			InvocationTargetException, InvalidArgumentException {
+
+		MountainSquare square = new MountainSquare(COLUMNS, ROWS);
+
+		Method canIncrease = MountainSquare.class.getDeclaredMethod("canIncrease", int.class, int.class);
+		canIncrease.setAccessible(true);
+
+		// 範囲内のすべての座標でテスト
+		for (int y = 0; y < ROWS; y++) {
+			for (int x = 0; x < COLUMNS; x++) {
+				assertTrue((boolean) canIncrease.invoke(square, x, y));
+			}
+		}
+	}
 }
